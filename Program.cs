be45c25396ab81450d8023de22b652cf0ff9a71c
Daniel Roth <daroth@microsoft.com>
2023-09-18 16:53:11 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Net8BlazorServer;
 using Net8BlazorServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents().AddServerComponents();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -25,7 +26,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>().AddServerRenderMode();
 
 app.Run();
